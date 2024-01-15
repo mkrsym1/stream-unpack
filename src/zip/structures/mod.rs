48fd5ce8919ea6c6a46fb299ab4e721b11a57df6
mkrsym1 <mkrsym1@gmail.com>
2024-01-15 20:12:17 +0200

    Fixed CompressionMethod::is_supported

--- src/zip/structures/mod.rs
+++ src/zip/structures/mod.rs
@@ -63,10 +63,6 @@ impl CompressionMethod {
 
     /// Returns whether decompression is supported for this method
     pub fn is_supported(&self) -> bool {
-        let Self::Unknown(..) = self else { 
-            return false; 
-        };
-
-        true
+        matches!(self, Self::Unknown(..))
     }
 }
